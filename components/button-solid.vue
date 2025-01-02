9ebc2b248a2e1f2dfed3155de0989f1d61539160
ponseltapos <ponseltapos456@gmail.com>
2025-01-02 15:20:52 +0700

    create hero section

---
+++ components/button-solid.vue
@@ -0,0 +1,5 @@
+<template>
+    <a href="#" class="rounded-lg bg-sky-800 hover:bg-sky-700 text-white lg:text-base text-sm popp-bold px-6 py-4">
+        <slot></slot>
+    </a>
+</template>
--- components/hero-section.vue
+++ components/hero-section.vue
@@ -1,20 +1,30 @@
+<script setup>
+import buttonSolid from './button-solid.vue';
+import buttonBOutline from './button-b-outline.vue';
+</script>
+
 <template>
-  <div class="h-screen lg:px-[120px] px-2 bg-gradient-to-b from-sky-100 via-sky-300 to-sky-500">
-    <div class="flex flex-col justify-end h-full">
-      <div class="flex flex-row">
-        <div class="flex flex-col w-1/2">
-          <h3 class="text-2xl mont-semibold text-sky-800 mb-6">HALLO, I’M KAMA</h3>
-          <h2 class="text-[35px] mont-bold text-sky-800 mb-14">I’M A FRONTEND DEVELOPER AND UI DESIGN</h2>
-          <p class="text-lg font-popp text-sky-800">
+  <div class="md:h-screen lg:pt-0 pt-24 min-[1105px]:px-[133px] md:px-16 px-8 bg-gradient-to-b from-sky-100 via-sky-300 to-sky-500">
+    <div class="flex flex-row h-full">
+        <div class="flex flex-col justify-center lg:w-1/2 md:w-3/4 w-full">
+          <h3 class="text-xl mont-semibold text-sky-800 mb-6">{{ $t("hero.hallo") }}</h3>
+          <h2 class="text-[30px] mont-bold text-sky-800 mb-12 whitespace-pre-line">{{ $t("hero.profession") }}</h2>
+          <p class="text-base font-popp text-sky-800 mb-12">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eius est eligendi ullam maxime earum
             excepturi repellat cupiditate quis labore laborum perferendis, corrupti, aut quidem?
           </p>
+          <div class="mb-4 flex sm:flex-none flex-col sm:flex-row">
+            <ButtonSolid class="w-fit mb-5 sm:mb-0">
+              {{ $t("hero.btn-porto") }}
+            </ButtonSolid>
+            <buttonBOutline class="lg:ms-12 sm:ms-7 ms-0 w-fit">
+              {{ $t("hero.btn-git") }}
+            </buttonBOutline>
+          </div>
         </div>
-
-        <div class="flex justify-end w-1/2">
-          <NuxtImg src="/foto-hero.png" alt="profile-photo" sizes="350px" />
+        <div class="md:flex hidden justify-end items-end md:w-1/2 w-1/4">
+          <NuxtImg src="/foto-hero.png" alt="profile-photo" class="lg:w-[360px] max-[1105px]:w-[300px] md:w-[250px] object-contain" />
         </div>
-      </div>
     </div>
   </div>
 </template>
