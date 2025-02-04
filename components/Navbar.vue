3e037278b967d6f94649ec3899af71d2d5b68c47
ponseltapos <ponseltapos456@gmail.com>
2025-02-04 14:42:47 +0700

    fix: active link navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,43 +1,24 @@
 <template>
   <nav class="relative">
-    <div
-      :class="{
-        'bg-opacity-50 backdrop-blur-lg w-full md:px-24 px-4': isScrolled && windowWidth >= 640,
-        'max-w-[1100px] px-6 lg:rounded-b-lg': !isScrolled,
-      }"
-      class="mx-auto fixed z-[999] top-0 left-0 right-0 bg-white rounded-b-none lg:drop-shadow transition-all duration-300"
-    >
+    <div :class="{
+      'bg-opacity-50 backdrop-blur-lg w-full md:px-24 px-4': isScrolled && windowWidth >= 640,
+      'max-w-[1100px] px-6 lg:rounded-b-lg': !isScrolled,
+    }"
+      class="mx-auto fixed z-[999] top-0 left-0 right-0 bg-white rounded-b-none lg:drop-shadow transition-all duration-300">
       <div class="relative flex h-16 items-center justify-between">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button-->
-          <button
-            type="button"
+          <button type="button"
             class="relative inline-flex items-center justify-center rounded-md p-2 text-sky-800 hover:bg-sky-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-            aria-controls="mobile-menu"
-            @click="toggleMenu('mobile')"
-          >
+            aria-controls="mobile-menu" @click="toggleMenu('mobile')">
             <span class="absolute -inset-0.5"></span>
             <span class="sr-only">Open main menu</span>
-            <svg
-              :class="menus.mobile ? 'hidden' : 'block'"
-              class="size-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
+            <svg :class="menus.mobile ? 'hidden' : 'block'" class="size-6" fill="none" viewBox="0 0 24 24"
+              stroke-width="1.5" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
-            <svg
-              :class="menus.mobile ? 'block' : 'hidden'"
-              class="size-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
+            <svg :class="menus.mobile ? 'block' : 'hidden'" class="size-6" fill="none" viewBox="0 0 24 24"
+              stroke-width="1.5" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
             </svg>
           </button>
@@ -46,52 +27,36 @@
         <!-- Navlink -->
         <div class="sm:flex hidden flex-1 items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex shrink-0 items-center">
-            <NuxtLink to="/">
+            <NuxtLink :to="getLocalizedLink('/')">
               <NuxtImg src="/images/logo-brand.png" alt="logo" class="h-8 object-contain" />
             </NuxtLink>
           </div>
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
-              <NuxtLink
-                to="/"
-                :class="{
-                  'text-black hover:border-sky-800': isScrolled && windowWidth >= 640,
-                  'text-sky-400 hover:border-sky-200': !isScrolled,
-                }"
-                class="px-3 py-2 text-sm mont-medium hover:border-b-2"
-                exact-active-class="border-b-2 border-sky-800 text-sky-800 mont-bold"
-                >{{ $t("navbar.home") }}
+              <NuxtLink :to="getLocalizedLink('/')" :class="{
+                'text-black hover:border-sky-800': isScrolled && windowWidth >= 640,
+                'text-sky-400 hover:border-sky-200': !isScrolled,
+              }" class="px-3 py-2 text-sm mont-medium hover:border-b-2"
+                exact-active-class="border-b-2 border-sky-800 text-sky-800 mont-bold">{{ $t("navbar.home") }}
               </NuxtLink>
-              <NuxtLink
-                to="/about-me"
-                :class="{
-                  'text-black hover:border-sky-800': isScrolled && windowWidth >= 640,
-                  'text-sky-400 hover:border-sky-200': !isScrolled,
-                }"
-                class="px-3 py-2 text-sm mont-medium hover:border-b-2 hover:border-sky-200"
-                exact-active-class="border-b-2 border-sky-800 text-sky-800 mont-bold"
-              >
+              <NuxtLink :to="getLocalizedLink('/about-me')" :class="{
+                'text-black hover:border-sky-800': isScrolled && windowWidth >= 640,
+                'text-sky-400 hover:border-sky-200': !isScrolled,
+              }" class="px-3 py-2 text-sm mont-medium hover:border-b-2 hover:border-sky-200"
+                exact-active-class="border-b-2 border-sky-800 text-sky-800 mont-bold">
                 {{ $t("navbar.about") }}
               </NuxtLink>
-              <NuxtLink
-                to="/portfolio"
-                :class="{
-                  'text-black hover:border-sky-800': isScrolled && windowWidth >= 640,
-                  'text-sky-400 hover:border-sky-200': !isScrolled,
-                }"
-                class="px-3 py-2 text-sm mont-medium hover:border-b-2 hover:border-sky-200"
-                exact-active-class="border-b-2 border-sky-800 text-sky-800 mont-bold"
-                >{{ $t("navbar.portfolio") }}
+              <NuxtLink :to="getLocalizedLink('/portfolio')" :class="{
+                'text-black hover:border-sky-800': isScrolled && windowWidth >= 640,
+                'text-sky-400 hover:border-sky-200': !isScrolled,
+              }" class="px-3 py-2 text-sm mont-medium hover:border-b-2 hover:border-sky-200"
+                exact-active-class="border-b-2 border-sky-800 text-sky-800 mont-bold">{{ $t("navbar.portfolio") }}
               </NuxtLink>
-              <NuxtLink
-                to="/contact"
-                :class="{
-                  'text-black hover:border-sky-800': isScrolled && windowWidth >= 640,
-                  'text-sky-400 hover:border-sky-200': !isScrolled,
-                }"
-                class="px-3 py-2 text-sm mont-medium hover:border-b-2 hover:border-sky-200"
-                exact-active-class="border-b-2 border-sky-800 text-sky-800 mont-bold"
-                >{{ $t("navbar.contact") }}
+              <NuxtLink :to="getLocalizedLink('/contact')" :class="{
+                'text-black hover:border-sky-800': isScrolled && windowWidth >= 640,
+                'text-sky-400 hover:border-sky-200': !isScrolled,
+              }" class="px-3 py-2 text-sm mont-medium hover:border-b-2 hover:border-sky-200"
+                exact-active-class="border-b-2 border-sky-800 text-sky-800 mont-bold">{{ $t("navbar.contact") }}
               </NuxtLink>
             </div>
           </div>
@@ -99,43 +64,25 @@
         <div class="absolute flex items-center inset-y-0 right-0 pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
           <!-- Language btn -->
           <div class="relative ml-3">
-            <button
-              type="button"
+            <button type="button"
               class="inline-flex items-center font-semibold justify-center px-4 py-2 text-sm text-sky-800 dark:text-white rounded-lg cursor-pointer hover:bg-gray-100 dark dark:hover:text-white"
-              @click="toggleLanguageMenu"
-            >
-              <svg
-                v-if="language === 'en'"
-                aria-hidden="true"
-                class="h-5 w-5 rounded-full me-2"
-                xmlns="http://www.w3.org/2000/svg"
-                id="flag-icon-css-us"
-                viewBox="0 0 512 512"
-              >
+              @click="toggleLanguageMenu">
+              <svg v-if="language === 'en'" aria-hidden="true" class="h-5 w-5 rounded-full me-2"
+                xmlns="http://www.w3.org/2000/svg" id="flag-icon-css-us" viewBox="0 0 512 512">
                 <g fill-rule="evenodd">
                   <g stroke-width="1pt">
-                    <path
-                      fill="#bd3d44"
+                    <path fill="#bd3d44"
                       d="M0 0h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
-                      transform="scale(3.9385)"
-                    />
-                    <path
-                      fill="#fff"
+                      transform="scale(3.9385)" />
+                    <path fill="#fff"
                       d="M0 10h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
-                      transform="scale(3.9385)"
-                    />
+                      transform="scale(3.9385)" />
                   </g>
                   <path fill="#192f5d" d="M0 0h98.8v70H0z" transform="scale(3.9385)" />
                 </g>
               </svg>
-              <svg
-                v-if="language === 'id'"
-                aria-hidden="true"
-                class="h-5 w-5 rounded-full me-2"
-                xmlns="http://www.w3.org/2000/svg"
-                id="flag-icon-css-id"
-                viewBox="0 0 512 512"
-              >
+              <svg v-if="language === 'id'" aria-hidden="true" class="h-5 w-5 rounded-full me-2"
+                xmlns="http://www.w3.org/2000/svg" id="flag-icon-css-id" viewBox="0 0 512 512">
                 <g fill-rule="evenodd">
                   <path fill="#e30a17" d="M0 0h512v256H0z" />
                   <path fill="#fff" d="M0 256h512v256H0z" />
@@ -144,39 +91,24 @@
               {{ language === "en" ? "English (US)" : "Indonesia" }}
             </button>
 
-            <div
-              v-if="menus.language"
+            <div v-if="menus.language"
               class="origin-top-right w-max absolute right-0 mt-5 shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-              role="menu"
-              aria-orientation="vertical"
-              aria-labelledby="options-menu"
-            >
+              role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
               <ul class="py-2 font-medium" role="none">
                 <li>
-                  <div
-                    @click="changeLanguage('en')"
+                  <div @click="changeLanguage('en')"
                     class="cursor-pointer px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-                    role="menuitem"
-                  >
+                    role="menuitem">
                     <div class="inline-flex items-center">
-                      <svg
-                        aria-hidden="true"
-                        class="h-3.5 w-3.5 rounded-full me-2"
-                        xmlns="http://www.w3.org/2000/svg"
-                        id="flag-icon-css-us"
-                        viewBox="0 0 512 512"
-                      >
+                      <svg aria-hidden="true" class="h-3.5 w-3.5 rounded-full me-2" xmlns="http://www.w3.org/2000/svg"
+                        id="flag-icon-css-us" viewBox="0 0 512 512">
                         <g fill-rule="evenodd">
-                          <path
-                            fill="#bd3d44"
+                          <path fill="#bd3d44"
                             d="M0 0h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
-                            transform="scale(3.9385)"
-                          />
-                          <path
-                            fill="#fff"
+                            transform="scale(3.9385)" />
+                          <path fill="#fff"
                             d="M0 10h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
-                            transform="scale(3.9385)"
-                          />
+                            transform="scale(3.9385)" />
                         </g>
                         <path fill="#192f5d" d="M0 0h98.8v70H0z" transform="scale(3.9385)" />
                       </svg>
@@ -185,19 +117,12 @@
                   </div>
                 </li>
                 <li>
-                  <div
-                    @click="changeLanguage('id')"
+                  <div @click="changeLanguage('id')"
                     class="cursor-pointer px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-                    role="menuitem"
-                  >
+                    role="menuitem">
                     <div class="inline-flex items-center">
-                      <svg
-                        aria-hidden="true"
-                        class="h-3.5 w-3.5 rounded-full me-2"
-                        xmlns="http://www.w3.org/2000/svg"
-                        id="flag-icon-css-id"
-                        viewBox="0 0 512 512"
-                      >
+                      <svg aria-hidden="true" class="h-3.5 w-3.5 rounded-full me-2" xmlns="http://www.w3.org/2000/svg"
+                        id="flag-icon-css-id" viewBox="0 0 512 512">
                         <g fill-rule="evenodd">
                           <path fill="#e30a17" d="M0 0h512v256H0z" />
                           <path fill="#fff" d="M0 256h512v256H0z" />
@@ -217,38 +142,18 @@
     <!-- Navlink Mobile -->
     <div v-if="menus.mobile" class="fixed sm:hidden bg-white w-full drop-shadow" id="mobile-menu">
       <div class="space-y-1 px-2 pb-3">
-        <a
-          href="/"
-          class="block rounded-md px-3 py-2 text-sm font-semibold text-sky-800"
-          :class="{
-            'bg-sky-200 mt-[70px]': isActive('/'),
-          }"
-          >{{ $t("navbar.home") }}</a
-        >
-        <a
-          href="/about-me"
-          class="block rounded-md px-3 py-2 text-sm font-semibold text-sky-800"
-          :class="{
-            'bg-sky-200': isActive('/about-me'),
-          }"
-          >{{ $t("navbar.about") }}</a
-        >
-        <a
-          href="/portfolio"
-          class="block rounded-md px-3 py-2 text-sm font-semibold text-sky-800"
-          :class="{
-            'bg-sky-200': isActive('/portfolio'),
-          }"
-          >{{ $t("navbar.portfolio") }}</a
-        >
-        <a
-          href="/contact"
-          class="block rounded-md px-3 py-2 text-sm font-semibold text-sky-800"
-          :class="{
-            'bg-sky-200': isActive('/contact'),
-          }"
-          >{{ $t("navbar.contact") }}</a
-        >
+        <a href="/" class="block rounded-md px-3 py-2 text-sm font-semibold text-sky-800" :class="{
+          'bg-sky-200 mt-[70px]': isActive('/'),
+        }">{{ $t("navbar.home") }}</a>
+        <a href="/about-me" class="block rounded-md px-3 py-2 text-sm font-semibold text-sky-800" :class="{
+          'bg-sky-200': isActive('/about-me'),
+        }">{{ $t("navbar.about") }}</a>
+        <a href="/portfolio" class="block rounded-md px-3 py-2 text-sm font-semibold text-sky-800" :class="{
+          'bg-sky-200': isActive('/portfolio'),
+        }">{{ $t("navbar.portfolio") }}</a>
+        <a href="/contact" class="block rounded-md px-3 py-2 text-sm font-semibold text-sky-800" :class="{
+          'bg-sky-200': isActive('/contact'),
+        }">{{ $t("navbar.contact") }}</a>
       </div>
     </div>
   </nav>
@@ -281,6 +186,11 @@ const toggleMenu = (menu) => {
 // i18n setup
 const { setLocale, locale } = useI18n();
 const language = ref(locale.value);
+
+// Mengambil prefix url
+const getLocalizedLink = (path) => {
+  return `/${locale.value}${path}`;
+}
 
 // Fungsi untuk mengubah bahasa
 const changeLanguage = (lang) => {
