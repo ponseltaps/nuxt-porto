f9a8678deca4640f19e175371b2c0848239313ce
ponseltapos <ponseltapos456@gmail.com>
2025-02-05 19:45:54 +0700

    create:skeleton load Tab edu & exp

--- components/about/professional-tab.vue
+++ components/about/professional-tab.vue
@@ -1,5 +1,34 @@
 <template>
-    <div class="min-[1105px]:px-[133px] md:px-16 px-8 mt-28 mb-28">
+    <!-- Skeleton Loader -->
+    <div v-if="loading" class="min-[1105px]:px-[133px] md:px-16 px-8 mt-28 mb-28">
+        <div class="animate-pulse">
+            <div class="h-10 w-full bg-sky-400 rounded-xl"></div>
+            <div class="h-16 w-full bg-sky-400 rounded-xl mt-8"></div>
+            <div v-if="mainActiveTab === 'experience'" id="experience-tab"
+                class="flex md:flex-row flex-col bg-sky-400 rounded-b-2xl py-12 md:mt-6 mt-3 px-4 gap-x-4">
+                <div class="md:flex hidden flex-col w-1/4 gap-y-4">
+                    <div class="h-8 bg-sky-300 rounded">
+                    </div>
+                    <div class="h-8 bg-sky-300 rounded">
+                    </div>
+                </div>
+
+                <div class="container-tab-content w-full">
+                    <div class="tab-content w-full md:border-l-2 pr-12 border-sky-300">
+                        <div class="flex flex-col mb-4 ml-4 space-y-2">
+                            <div class="h-8 w-1/2 bg-sky-300 rounded"></div>
+                            <div class="h-6 w-1/3 bg-sky-300 rounded"></div>
+                            <div class="h-6 w-1/3 bg-sky-300 rounded"></div>
+                        </div>
+
+                        <div class="ml-4 md:h-44 h-24 w-full bg-sky-300 rounded"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div v-else class="min-[1105px]:px-[133px] md:px-16 px-8 mt-28 mb-28">
         <div>
             <h2 class="text-3xl text-center text-sky-800 mont-bold uppercase">
                 {{ $t("about.tab.header") }}
@@ -38,29 +67,23 @@
             <!-- Content experience tab -->
             <div v-if="mainActiveTab === 'experience'" id="experience-tab"
                 class="flex md:flex-row flex-col bg-sky-950 rounded-b-2xl py-12 md:mt-6 mt-3">
-
                 <div class="tab-btn">
                     <div class="link-tabs flex flex-col items-start w-max mr-12 space-y-4 border-l-4">
-                        <!-- Tombol ini hanya akan muncul jika data experienceTabs ada -->
                         <button v-if="experienceTabs && experienceTabs.length" v-for="tab in experienceTabs"
                             :key="tab.expTab" @click="experienceActiveTab = tab.expTab">
-                            <!-- expTab nanti akan diubah menjadi slug dari database -->
                             <div :class="{ 'border-sky-400': experienceActiveTab === tab.expTab }"
                                 class="border-l-4 py-2 px-5 -ml-1">
                                 <p :class="{ 'text-sky-400': experienceActiveTab === tab.expTab }"
                                     class="text-sky-200 text-base popp-bold">
-                                    <!-- Menampilkan data dari 'company' -->
                                     {{ tab.name }}
                                 </p>
                             </div>
                         </button>
                     </div>
                 </div>
-
                 <div class="block md:hidden py-5">
                     <hr class="border-1 border-sky-400 " />
                 </div>
-
                 <div class="container-tab-content">
                     <div v-if="experienceTabs && experienceTabs.length" v-for="tab in experienceTabs"
                         :key="tab.expTab + '-experience'">
@@ -85,6 +108,7 @@
                     </div>
                 </div>
             </div>
+
             <!-- Content education tab -->
             <div v-if="mainActiveTab === 'education'" id="education-tab"
                 class="flex md:flex-row flex-col bg-sky-950 rounded-b-2xl py-12 md:mt-6 mt-3">
@@ -92,12 +116,11 @@
                     <div class="link-tabs flex flex-col items-start w-max mr-12 space-y-4 border-l-4">
                         <button v-if="educationTabs.length" v-for="tabEdu in educationTabs" :key="tabEdu.eduTab"
                             @click="educationActiveTab = tabEdu.eduTab">
-                            <!-- Berisi tombol nama pendidikan -->
                             <div :class="{ 'border-sky-400': educationActiveTab === tabEdu.eduTab }"
                                 class="border-l-4 py-2 px-5 -ml-1">
                                 <p :class="{ 'text-sky-400': educationActiveTab === tabEdu.eduTab }"
                                     class="text-sky-200 text-base popp-bold">
-                                    {{ tabEdu.name }} <!-- Nama pendidikan -->
+                                    {{ tabEdu.name }}
                                 </p>
                             </div>
                         </button>
@@ -108,26 +131,21 @@
                 </div>
                 <div class="container-tab-content">
                     <div class="education-content">
-                        <!-- eduTab berdasarkan 'slug' -->
                         <div v-if="educationTabs.length" v-for="tab in educationTabs" :key="tab.eduTab + '-education'">
                             <div v-if="educationActiveTab === tab.eduTab"
                                 class="tab-content md:border-l-2 pr-12 border-sky-400">
                                 <div class="flex flex-col text-sky-400 mb-4 ml-4 space-y-1">
                                     <h3 class="popp-bold text-xl">
-                                        <!-- Menampilkan data 'education_name' -->
                                         {{ tab.name }}
                                     </h3>
                                     <p class="popp-medium text-sm">
-                                        <!-- Menampilkan data 'degree_level' -->
                                         {{ tab.degree }}
                                     </p>
                                     <p class="popp-medium text-sm">
-                                        <!-- Menampilkan data 'location' -->
                                         {{ tab.location }}
                                     </p>
                                 </div>
                                 <div class="ml-4">
-                                    <!-- Menampilkan deskripsi pembelajaran berdasarkan bahasa -->
                                     <div v-html="tab.description"></div>
                                 </div>
                             </div>
@@ -136,7 +154,6 @@
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 </template>
@@ -151,23 +168,24 @@ const language = ref(locale.value);
 
 const experienceTabs = ref([]);
 const jobDescriptions = ref({});
+const educationTabs = ref([]);
+const educationActiveTab = ref('');
 
 const experienceActiveTab = ref('');
-
 const mainActiveTab = ref('experience');
+
+const loading = ref(true); // Loading state to manage skeleton loader visibility
 
 const { $apiFetch } = useNuxtApp();
 
 const formatDate = (dateString) => {
     const date = new Date(dateString);
-
     if (isNaN(date)) {
         console.error(`Invalid date string: ${dateString}`);
         return dateString;
     }
-
     return new Intl.DateTimeFormat(locale.value, {
-        // day: 'numeric',
+        day: 'numeric',
         month: 'long',
         year: 'numeric',
     }).format(date);
@@ -175,68 +193,56 @@ const formatDate = (dateString) => {
 
 onMounted(async () => {
     try {
-        const response = await $apiFetch("/api/about/experience");
+        // Use Promise.all to wait for both requests (experience & education)
+        const [experienceResponse, educationResponse] = await Promise.all([
+            $apiFetch("/api/about/experience"),
+            $apiFetch("/api/about/education")
+        ]);
 
-        if (!response.ok) {
+        if (!experienceResponse.ok || !educationResponse.ok) {
             throw new Error("Network response was not ok");
         }
-        const data = await response.json();
 
-        if (data && Array.isArray(data) && data.length > 0) {
-            experienceTabs.value = data.map(item => ({
+        // Handle experience data
+        const experienceData = await experienceResponse.json();
+        if (experienceData && Array.isArray(experienceData) && experienceData.length > 0) {
+            experienceTabs.value = experienceData.map(item => ({
                 name: item.company,
                 expTab: item.slug,
                 position: item.position,
                 period: `${formatDate(item.start_priode)} - ${formatDate(item.end_priode)}`
             }));
-
-            data.forEach(item => {
+            experienceData.forEach(item => {
                 jobDescriptions.value[item.slug] = {
                     en: item.jobdesc_en,
                     id: item.jobdesc_id
                 };
             });
-
             if (experienceTabs.value.length > 0) {
                 experienceActiveTab.value = experienceTabs.value[0].expTab;
             }
         }
-    } catch (error) {
-        console.error('Error fetching experience data:', error);
-    }
-});
 
-
-const educationTabs = ref([]); // Untuk menampung data pendidikan
-const educationActiveTab = ref(''); // Untuk menentukan tab pendidikan yang aktif
-
-onMounted(async () => {
-    try {
-        // Ambil data pendidikan dari API
-        const response = await $apiFetch("/api/about/education");
-
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-
-        const data = await response.json();
-
-        if (data && Array.isArray(data) && data.length > 0) {
-            educationTabs.value = data.map(item => ({
-                name: item.education_name,  // Nama pendidikan
-                eduTab: item.slug,          // ID tab pendidikan (slug)
-                degree: item.degree_level,  // Tingkat pendidikan (degree level)
-                location: item.location,    // Lokasi pendidikan
-                description: language.value === "en" ? item.learning_desc_en : item.learning_desc_id, // Deskripsi pembelajaran
+        // Handle education data
+        const educationData = await educationResponse.json();
+        if (educationData && Array.isArray(educationData) && educationData.length > 0) {
+            educationTabs.value = educationData.map(item => ({
+                name: item.education_name,
+                eduTab: item.slug,
+                degree: item.degree_level,
+                location: item.location,
+                description: language.value === 'en' ? item.learning_desc_en : item.learning_desc_id
             }));
-
             if (educationTabs.value.length > 0) {
-                educationActiveTab.value = educationTabs.value[0].eduTab;  // Set tab pendidikan aktif pertama
+                educationActiveTab.value = educationTabs.value[0].eduTab;
             }
         }
+
     } catch (error) {
-        console.error('Error fetching education data:', error);
+        console.error('Error fetching data:', error);
+    } finally {
+        // Set loading to false once both requests are completed
+        loading.value = false;
     }
 });
-
 </script>
--- components/footer.vue
+++ components/footer.vue
@@ -6,51 +6,39 @@
         <p class="text-sky-800 text-2xl mont-bold">KamaDev</p>
       </div>
       <div class="flex flex-row justify-center md:justify-end items-center md:mt-0 mt-7 md:grow grow-0">
-        <NuxtLink to="/" class="px-3 py-2 text-sm mont-semibold text-sky-800" aria-current="page"
-          >{{ $t("navbar.home") }}
+        <NuxtLink to="/" class="px-3 py-2 text-sm mont-semibold text-sky-800" aria-current="page">{{ $t("navbar.home")
+          }}
         </NuxtLink>
-        <NuxtLink to="/about-me" class="px-3 py-2 text-sm mont-semibold text-sky-800">{{ $t("navbar.about") }} </NuxtLink>
-        <NuxtLink to="/portfolio" class="px-3 py-2 text-sm mont-semibold text-sky-800">{{ $t("navbar.portfolio") }} </NuxtLink>
-        <NuxtLink to="/contact" class="px-3 py-2 text-sm mont-semibold text-sky-800">{{ $t("navbar.contact") }}</NuxtLink>
+        <NuxtLink to="/about-me" class="px-3 py-2 text-sm mont-semibold text-sky-800">{{ $t("navbar.about") }}
+        </NuxtLink>
+        <NuxtLink to="/portfolio" class="px-3 py-2 text-sm mont-semibold text-sky-800">{{ $t("navbar.portfolio") }}
+        </NuxtLink>
+        <NuxtLink to="/contact" class="px-3 py-2 text-sm mont-semibold text-sky-800">{{ $t("navbar.contact") }}
+        </NuxtLink>
       </div>
     </div>
     <div class="border-b-2 border-sky-800 opacity-50"></div>
     <div class="flex flex-col justify-center items-center mt-7">
       <div class="flex flex-row mb-7 gap-x-8">
         <a href="https://linktr.ee/kamarezaporto" target="_blank">
-          <NuxtImg
-            src="/images/website-icon.svg"
-            alt="btn-icon"
-            class="md:w-7 w-5 object-contain hover:scale-125 ease-in-out duration-300"
-          />
+          <NuxtImg src="/images/website-icon.svg" alt="btn-icon"
+            class="md:w-7 w-5 object-contain hover:scale-125 ease-in-out duration-300" />
         </a>
         <a href="mailto:kamareza.worker@gmail.com">
-          <NuxtImg
-            src="/images/mail-icon.svg"
-            alt="btn-icon"
-            class="md:w-7 w-5 object-contain hover:scale-125 ease-in-out duration-300"
-          />
+          <NuxtImg src="/images/mail-icon.svg" alt="btn-icon"
+            class="md:w-7 w-5 object-contain hover:scale-125 ease-in-out duration-300" />
         </a>
         <a href="wa.me/6285156287045" target="_blank">
-          <NuxtImg
-            src="/images/whatsapp-icon.svg"
-            alt="btn-icon"
-            class="md:w-7 w-5 object-contain hover:scale-125 ease-in-out duration-300"
-          />
+          <NuxtImg src="/images/whatsapp-icon.svg" alt="btn-icon"
+            class="md:w-7 w-5 object-contain hover:scale-125 ease-in-out duration-300" />
         </a>
         <a href="https://www.linkedin.com/in/kamareza-muhammad-rizqy-1275291b2" target="_blank">
-          <NuxtImg
-            src="/images/linkedin-icon.svg"
-            alt="btn-icon"
-            class="md:w-7 w-5 object-contain hover:scale-125 ease-in-out duration-300"
-          />
+          <NuxtImg src="/images/linkedin-icon.svg" alt="btn-icon"
+            class="md:w-7 w-5 object-contain hover:scale-125 ease-in-out duration-300" />
         </a>
         <a href="https://id.pinterest.com/NotFound_12/" target="_blank">
-          <NuxtImg
-            src="/images/pinterest-icon.svg"
-            alt="btn-icon"
-            class="md:w-7 w-5 object-contain hover:scale-125 ease-in-out duration-300"
-          />
+          <NuxtImg src="/images/pinterest-icon.svg" alt="btn-icon"
+            class="md:w-7 w-5 object-contain hover:scale-125 ease-in-out duration-300" />
         </a>
       </div>
       <div class="text-sm text-sky-800 font-mont">
@@ -62,3 +50,29 @@
     </div>
   </footer>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const snsButton = ref([]);
+
+const { $apiFetch } = useNuxtApp();
+onMounted(async () => {
+  try {
+    const servicesResponse = await $apiFetch("/api/setting/sns");
+    if (!servicesResponse.ok) {
+      throw new Error("Network response for services was not ok");
+    }
+    snsButton.value = await servicesResponse.json();
+  } catch (err) {
+    console.error("Error fetching data:", err);
+  } finally {
+    loading.value = false;
+  }
+});
+
+// Fungsi untuk mendapatkan URL gambar
+const getImageUrl = (imagePath) => {
+  return `${useRuntimeConfig().public.apiBaseUrl}/storage/${imagePath}`;
+};
+</script>
